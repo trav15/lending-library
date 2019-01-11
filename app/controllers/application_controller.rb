f73b9d62c4162fb1c07492fffc92c529a2c9d3b8
trav15 <travistokuyama@gmail.com>
2019-01-10 22:01:53 -0500

    wire up new item action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,23 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
