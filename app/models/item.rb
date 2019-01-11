8eab1f50a2ee0e29c1127ec57693bc4b95afc983
trav15 <travistokuyama@gmail.com>
2019-01-10 20:20:07 -0500

    add item's default value true for available attribute

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_many :lends
   has_many :users, through: :lends
+
+  before_save :default_values
+
+  def default_values
+    self.available |= true
+  end
 end
