cbb6a020c0d12fc345f4a93b35b7faf652ac0718
trav15 <travistokuyama@gmail.com>
2019-01-11 16:37:16 -0500

    add bootstrap styling to flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:message] = "Successfully logged in!"
       redirect_to root_path
     else
       flash[:errors] = "Something went wrong!"
