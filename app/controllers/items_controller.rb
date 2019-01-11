82b1837255c4d6e37260420568f44fd595b48bd3
trav15 <travistokuyama@gmail.com>
2019-01-10 23:29:39 -0500

    fix index and show issues with strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :redirect_if_not_authorized, only: [:edit, :update, :destroy]
+  before_action :redirect_if_not_authorized!, only: [:edit, :update, :destroy]
 
   def index
+    if params[:user_id]
+      @user = User.find_by(params[:used_id])
+      @items = @user.items
+    else
+      @items = Item.all
+    end
   end
 
   def new
@@ -12,7 +18,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.create(item_params)
     if @item.save
-      flash[message] = "Item successfully donated!  Thank you!"
+      flash[:message] = "Item successfully donated!  Thank you!"
       redirect_to item_path(@item)
     else
       render :new
@@ -26,6 +32,7 @@ class ItemsController < ApplicationController
     if @item.user = current_user
     else
       redirect_to '/items'
+    end
   end
 
   def update
@@ -45,16 +52,18 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name)
+    params.require(:item).permit(:name, :created_at)
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find_by(id: params[:id])
   end
 
   def redirect_if_not_authorized!
     if @item.user != current_user
       redirect_to '/items'
+    else
+      redirect_to '/edit'
     end
   end
 end
