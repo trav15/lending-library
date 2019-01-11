510e56d18c4dd98dc11a9825d4deb144cf4dd2df
trav15 <travistokuyama@gmail.com>
2019-01-10 21:27:32 -0500

    add routes for controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
+  root to: 'items#index'
+
+  resources :items
+
+  resources :users, only: [:new, :create, :show] do
+    resources :lends, only: [:create]
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
