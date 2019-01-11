255177fa65c24b7d56fc5e684da7ede8718edf55
trav15 <travistokuyama@gmail.com>
2019-01-10 23:37:40 -0500

    correct Item column to include donor id

--- db/migrate/20190111005617_create_items.rb
+++ db/migrate/20190111005617_create_items.rb
@@ -2,6 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
+      t.integer :donor_id
       t.boolean :available
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_01_11_005617) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
+    t.integer "donor_id"
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
