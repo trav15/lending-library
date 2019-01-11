67d82cb55a894e48616c4972965139d8ef523516
trav15 <travistokuyama@gmail.com>
2019-01-10 19:56:44 -0500

    add Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_005049) do
+ActiveRecord::Schema.define(version: 2019_01_11_005617) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lends", force: :cascade do |t|
+    t.datetime "lend_date"
+    t.datetime "return_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
