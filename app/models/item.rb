ba97774c8b91a3a2e8a6c8761f80c70782b6c039
trav15 <travistokuyama@gmail.com>
2019-01-11 22:27:35 -0500

    remove before_save setting available to true

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,6 @@ class Item < ApplicationRecord
   has_many :lends
   has_many :users, through: :lends
 
-  before_save :default_values
   validates :name, presence: true
 
-  private
-  def default_values
-    self.available |= true
-  end
 end
