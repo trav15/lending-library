d26aac0cf7f1cd56b012f53bb57b68c4db4f6535
trav15 <travistokuyama@gmail.com>
2019-01-15 19:09:50 -0500

    fix issue with login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       flash[:message] = "Successfully logged in via Facebook!"
       redirect_to root_path
     else
-      @user = User.find_by_or_create_by(username: params[:username])
+      @user = User.find_or_create_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         flash[:message] = "Successfully logged in!"
