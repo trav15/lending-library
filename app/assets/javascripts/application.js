fbf2ae192f3a095068c4ff1761911bd88cd93808
trav15 <travistokuyama@gmail.com>
2019-01-23 22:58:59 -0500

    add loan count to item show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,6 @@ const bindClickHandlers = () => {
   //item index
   $('.all-items').on('click', (e) => {
     e.preventDefault();
-    // history.pushState(null, null, "items")
-    alert("HELLO")
     fetch('/items.json')
       .then(res => res.json())
       .then(items => {
@@ -40,7 +38,6 @@ const bindClickHandlers = () => {
   $(document).on('click', '.show-link', function(e) {
     e.preventDefault();
     let id = $(this).attr('data-id')
-    // history.pushState(null, null, `items/${id}`)
     fetch(`/items/${id}.json`)
       .then(res => res.json())
       .then(item => {
@@ -59,7 +56,6 @@ const bindClickHandlers = () => {
       .then(item => {
       $('.app-container').html('')
       let newItem = new Item(item)
-      // history.pushState(null, null, `../items/${item.id}`)
       let itemHtml = newItem.formatShow()
       $('.app-container').append(itemHtml)
     })
@@ -71,6 +67,7 @@ function Item(item) {
   this.name = item.name
   this.available = item.available
   this.created_at = item.created_at
+  this.loan_count = item.loans.length
 }
 
 Item.prototype.formatIndex = function() {
@@ -91,7 +88,8 @@ Item.prototype.formatShow = function() {
   let itemHtml = `
     <h3>${this.name}</h3>
     ${itemAvailablity}<br>
-    <button data-id="${this.id}" class="next-item">Next Item</button>
+    <p>Times borrowed: ${this.loan_count}</p><br>
+    <button data-id="${this.id}" class="next-item badge badge-secondary">Next Item</button>
   `
   return itemHtml
 }
